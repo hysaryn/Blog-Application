a36babcbc7573fc8caa05ff02dfc3e268f1bf573
yolandahe <he.yang7@northeastern.edu>
2024-04-03 15:10:07 -0700

    customize validation error message

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -2,14 +2,21 @@ package com.springboot.blog.exception;
 
 import com.springboot.blog.payload.ErrorDetails;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   //handle specific exceptions
   @ExceptionHandler(ResourceNotFoundException.class)
@@ -38,5 +45,16 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+      HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError)error).getField();
+      String message = error.getDefaultMessage();
+      errors.put(fieldName, message);
+    });
 
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
 }
