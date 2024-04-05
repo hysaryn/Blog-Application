0ce57366b4e38fbe4b9e649e3325f8d570ad8f00
yolandahe <he.yang7@northeastern.edu>
2024-04-04 23:06:23 -0700

    add register REST API

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.springboot.blog.config;
 
+import com.springboot.blog.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
