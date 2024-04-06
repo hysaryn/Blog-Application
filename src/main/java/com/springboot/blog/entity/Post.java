9d14472eab85c5a141af3bd116bcc149dff61e96
yolandahe <he.yang7@northeastern.edu>
2024-04-05 20:57:42 -0700

    create category JPA entity

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -3,9 +3,12 @@ package com.springboot.blog.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -44,4 +47,8 @@ public class Post {
 
   @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
   private Set<Comment> comments = new HashSet<>();
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id")
+  private Category category;
 }
--- src/main/java/com/springboot/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/springboot/blog/security/JwtAuthenticationFilter.java
@@ -26,8 +26,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   }
 
   @Override
-  protected void doFilterInternal(HttpServletRequest request,
-      HttpServletResponse response,
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
 
     //get JWT token from HTTP request
